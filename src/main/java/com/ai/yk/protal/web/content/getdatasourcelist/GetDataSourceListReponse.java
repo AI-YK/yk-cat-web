2cd2f5d86e6e4ef6976901e7d2270c16af458d64
xuyw <xuyw@xuyw-PC>
2016-12-06 10:14:30 +0800

    修改名称

--- src/main/java/com/ai/yk/protal/web/content/getdatasourcelist/GetDataSourceListReponse.java
+++ src/main/java/com/ai/yk/protal/web/content/getdatasourcelist/GetDataSourceListReponse.java
@@ -6,7 +6,7 @@ import com.ai.yk.protal.web.content.YJBaseRequest;
  * @author mengbo 
  *
  */
-public class GetDataSourceListRequest extends YJBaseRequest {
+public class GetDataSourceListReponse extends YJBaseRequest {
 
 	private GetDataSourceListMessage message;
 
