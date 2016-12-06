0559852cd2834d366b0266e0bc1329b6b0e25d1c
lixiang <lixiang@10.1.52.84>
2016-12-06 21:08:48 +0800

    q1q

--- src/main/java/com/ai/yk/protal/web/controller/emergency/EmergencyController.java
+++ src/main/java/com/ai/yk/protal/web/controller/emergency/EmergencyController.java
@@ -1,5 +1,8 @@
 package com.ai.yk.protal.web.controller.emergency;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,17 +11,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ai.opt.sdk.web.model.ResponseData;
-import com.ai.paas.ipaas.i18n.ResWebBundle;
-import com.ai.yk.protal.web.controller.IndexController;
+import com.ai.yk.protal.web.content.queryeventdataList.QueryEventDataListReponse;
+import com.ai.yk.protal.web.content.queryeventdataList.QueryEventDataVo;
+import com.ai.yk.protal.web.service.eventdata.EventDataService;
+import com.alibaba.fastjson.JSONObject;
 
 @Controller
 public class EmergencyController {
 /**
  * 突发事件	
 	 */
-		 private static final Logger LOGGER = LoggerFactory.getLogger(IndexController.class);
+		 private static final Logger LOGGER = LoggerFactory.getLogger(EmergencyController.class);
 		    @Autowired
-		    ResWebBundle rb;
+		    EventDataService eventDataService;
 	    /**
 	     * 突发事件前四条列表
 	     * @return
@@ -26,7 +31,26 @@ public class EmergencyController {
 	    @RequestMapping("/getEmergencyIndexList")
 	    @ResponseBody
 	    public ResponseData<String> getEmergencyIndexList(){ 
-	        return new ResponseData<String>(ResponseData.AJAX_STATUS_SUCCESS, "", "1223");
+	    	QueryEventDataVo queryEventDataVo =new QueryEventDataVo();
+	    	queryEventDataVo.setId(1);
+	    	queryEventDataVo.setZhCountry("中国");
+	    	queryEventDataVo.setZhCity("北京");
+	    	queryEventDataVo.setDayTime("2016-12-06");
+	    	queryEventDataVo.setZhTitle("朴槿惠支持率暴跌至5%，就亲信干政事件道歉支");
+	    	List<QueryEventDataVo> results = new ArrayList<QueryEventDataVo>();
+	    	results.add(queryEventDataVo);
+	    	results.add(queryEventDataVo);
+	    	results.add(queryEventDataVo);
+	    	results.add(queryEventDataVo);
+	    	QueryEventDataListReponse queryEventDataListReponse =new QueryEventDataListReponse();
+	    	queryEventDataListReponse.setIndustryType(1);
+	    	queryEventDataListReponse.setPageNo(1);
+	    	queryEventDataListReponse.setPageSize(4);
+	    	queryEventDataListReponse.setResults(results);
+	    	queryEventDataListReponse.setSourceType(1);
+	    	queryEventDataListReponse.setType(1);
+	    	String json = JSONObject.toJSONString(queryEventDataListReponse);
+	    	return new ResponseData<String>("","","");
 	        
 	    }
 }
