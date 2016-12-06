73ce948aee86a7f609f8a1c0dfdd7361ced8b424
lenovo <lenovo@10.1.52.87>
2016-12-06 14:14:37 +0800

    社交详情出参如初

---
+++ src/main/java/com/ai/yk/protal/web/content/socialdetail/SocialDetailRequest.java
@@ -0,0 +1,28 @@
+package com.ai.yk.protal.web.content.socialdetail;
+
+import com.ai.yk.protal.web.content.YJBaseRequest;
+/**
+ *社交详情接口入参
+ * @author shancong
+ *
+ */
+public class SocialDetailRequest extends YJBaseRequest {
+
+	private SocialDetailMessage message;
+
+	public SocialDetailMessage getMessage() {
+		return message;
+	}
+
+	public void setMessage(SocialDetailMessage message) {
+		this.message = message;
+	}
+	
+	
+
+
+
+	
+
+	
+}
---
+++ src/main/java/com/ai/yk/protal/web/content/socialdetail/SocialDetailResponse.java
@@ -0,0 +1,23 @@
+package com.ai.yk.protal.web.content.socialdetail;
+
+import com.ai.yk.protal.web.content.YJBaseResponse;
+/**
+ * 社交详情接口出参
+ * @author shancong
+ *
+ */
+public class SocialDetailResponse extends YJBaseResponse{
+
+	private SocialDetailData data;
+
+	public SocialDetailData getData() {
+		return data;
+	}
+
+	public void setData(SocialDetailData data) {
+		this.data = data;
+	}
+
+	
+	
+}
