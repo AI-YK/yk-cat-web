c0b34d7b2c7cf8c0c75be7691d4cf091dcc8f80c
xuyw <xuyw@xuyw-PC>
2016-12-07 13:46:20 +0800

    提交社交详情

--- src/main/java/com/ai/yk/protal/web/content/socialdetail/SocialDetailMessage.java
+++ src/main/java/com/ai/yk/protal/web/content/socialdetail/SocialDetailMessage.java
@@ -1,6 +1,10 @@
 package com.ai.yk.protal.web.content.socialdetail;
-public class SocialDetailMessage{
+
+import java.io.Serializable;
+
+public class SocialDetailMessage implements Serializable{
 	
+	private static final long serialVersionUID = -6360190606915084713L;
 	/**资讯id   是否必填：Y**/
 	private String informationId;
 
---
+++ src/main/java/com/ai/yk/protal/web/service/social/SocialService.java
@@ -0,0 +1,28 @@
+package com.ai.yk.protal.web.service.social;
+
+import org.springframework.stereotype.Service;
+
+import com.ai.opt.sdk.util.StringUtil;
+import com.ai.yk.protal.web.constants.YeesightApiUrlConstants;
+import com.ai.yk.protal.web.content.YJRequest;
+import com.ai.yk.protal.web.content.YJResponse;
+import com.ai.yk.protal.web.content.socialdetail.SocialDetailMessage;
+import com.ai.yk.protal.web.content.socialdetail.SocialDetailResponse;
+import com.ai.yk.protal.web.utils.HttpClientUtil;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+
+@Service
+public class SocialService {
+	/**
+	 * 查询社交详情
+	 */
+	public YJResponse<SocialDetailResponse> queryNewsDetail(YJRequest<SocialDetailMessage> req) {
+		String url = YeesightApiUrlConstants.getApiUrl(YeesightApiUrlConstants.API_INFODETAIL_SOCIALDETAIL);
+		String result =HttpClientUtil.getYJBaseResponse(url,req);
+		if(!StringUtil.isBlank(result)){
+		 return	JSON.parseObject(result, new TypeReference<YJResponse<SocialDetailResponse>>(){});
+		}
+		return null;
+	}
+}
