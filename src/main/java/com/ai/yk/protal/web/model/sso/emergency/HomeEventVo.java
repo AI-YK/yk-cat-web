d9132bb6f1df63ba7ffaec64b85d34803406599e
lixiang <lixiang@lixiang-PC>
2016-12-07 11:37:13 +0800

    home

---
+++ src/main/java/com/ai/yk/protal/web/model/sso/emergency/HomeEventVo.java
@@ -0,0 +1,41 @@
+package com.ai.yk.protal.web.model.sso.emergency;
+
+import java.io.Serializable;
+import java.util.List;
+
+import com.ai.yk.protal.web.content.event.EventVo;
+
+public class HomeEventVo implements Serializable{
+	
+	/**
+	 * 突发事件（列表和chart）
+	 */
+	private static final long serialVersionUID = 1L;
+	/**
+	 *突发事件列表
+	 */
+	private List<EventVo> eventList;
+	/**
+	 * chart集合
+	 */
+	private List<ChartGroup> group;
+	
+	public List<EventVo> getEventList() {
+		return eventList;
+	}
+
+	public void setEventList(List<EventVo> eventList) {
+		this.eventList = eventList;
+	}
+
+	public List<ChartGroup> getGroup() {
+		return group;
+	}
+
+	public void setGroup(List<ChartGroup> group) {
+		this.group = group;
+	}
+	
+	
+
+}
