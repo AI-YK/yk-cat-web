639940180f0750f6926af74c2330a34bf7332732
lenovo <lenovo@lenovo-pc>
2016-12-08 16:21:40 +0800

    更改注释

--- src/main/java/com/ai/yk/protal/web/content/searchPublicSafety/SearchPublicSafetyMessage.java
+++ src/main/java/com/ai/yk/protal/web/content/searchPublicSafety/SearchPublicSafetyMessage.java
@@ -7,14 +7,25 @@ import java.io.Serializable;
  */
 public class SearchPublicSafetyMessage implements Serializable {
 	private static final long serialVersionUID = -3627850423820477791L;
+		/**媒体类型**/
 		private String mediaType;
+		/**情感ID(1正面，0：中性 -1负面)**/
 		private String sentimentId;
+		/**省份**/
 		private String province;
+		/**城市**/
 		private String city;
+		/**舆情分类类型（多个用逗号隔开）**/
 		private String publicAffairsType;
+		/**数据源类型微信微博新闻等媒体**/
 		private String mediaId;
+		/**媒体级别id**/
 		private String mediaLevel;
+		/**指定排序字段**/
 		private String fieldName;
+		/**排序字段
+		_score：相关度,pubdate：时间，权重mediaLevel, 转载量transfer
+		**/
 		private String order;
 		public String getMediaType() {
 			return mediaType;
