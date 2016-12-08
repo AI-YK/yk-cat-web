75579287b688d75f7cacb7c22b195bd2f4f530e5
xuyw <xuyw@xuyw-PC>
2016-12-08 21:03:46 +0800

    数据字典测试

---
+++ src/main/test/dic/DicTest.java
@@ -0,0 +1,22 @@
+package dic;
+
+import com.ai.yk.protal.web.content.YJRequest;
+import com.ai.yk.protal.web.content.common.DicMessage;
+import com.ai.yk.protal.web.service.common.CommonService;
+import com.alibaba.fastjson.JSON;
+
+public class DicTest {
+
+	public static void main(String[] args) {
+		YJRequest<DicMessage> req = new YJRequest<>();
+		DicMessage m = new DicMessage();
+		m.setLanguage("zh");
+		m.setType("SJLY");
+		req.setMessage(m);
+		CommonService test = new CommonService();
+		System.out.println(JSON.toJSONString(test
+				.queryDicByTypeAndLanguageForNews(req)));
+
+	}
+
+}
