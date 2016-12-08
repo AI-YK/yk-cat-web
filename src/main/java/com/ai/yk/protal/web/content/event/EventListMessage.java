66d363be45f1976f8d5fc1d5a33e915e9fb830ab
xuyw <xuyw@xuyw-PC>
2016-12-08 11:30:20 +0800

    提交

--- src/main/java/com/ai/yk/protal/web/content/event/EventListMessage.java
+++ src/main/java/com/ai/yk/protal/web/content/event/EventListMessage.java
@@ -14,63 +14,55 @@ public class EventListMessage implements Serializable {
 	private Integer industryType;
 	// 数据类型（ 1 : 新闻 2 : 社交）
 	private Integer sourceType;
-	private String country;
-	private String city;
-
+	private String countryCode ;
+	private String provinceCode;
+	private String cityCode;
 	public Integer getPageSize() {
 		return pageSize;
 	}
-
 	public void setPageSize(Integer pageSize) {
 		this.pageSize = pageSize;
 	}
-
 	public Integer getPageNo() {
 		return pageNo;
 	}
-
 	public void setPageNo(Integer pageNo) {
 		this.pageNo = pageNo;
 	}
-
 	public Integer getType() {
 		return type;
 	}
-
 	public void setType(Integer type) {
 		this.type = type;
 	}
-
 	public Integer getIndustryType() {
 		return industryType;
 	}
-
 	public void setIndustryType(Integer industryType) {
 		this.industryType = industryType;
 	}
-
 	public Integer getSourceType() {
 		return sourceType;
 	}
-
 	public void setSourceType(Integer sourceType) {
 		this.sourceType = sourceType;
 	}
-
-	public String getCountry() {
-		return country;
+	public String getCountryCode() {
+		return countryCode;
 	}
-
-	public void setCountry(String country) {
-		this.country = country;
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
 	}
-
-	public String getCity() {
-		return city;
+	public String getProvinceCode() {
+		return provinceCode;
 	}
-
-	public void setCity(String city) {
-		this.city = city;
+	public void setProvinceCode(String provinceCode) {
+		this.provinceCode = provinceCode;
 	}
-
+	public String getCityCode() {
+		return cityCode;
+	}
+	public void setCityCode(String cityCode) {
+		this.cityCode = cityCode;
+	}
 }
