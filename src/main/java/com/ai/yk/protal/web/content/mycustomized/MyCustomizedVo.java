07230bf5a1d413fcd9ecee3b77e31901c640c096
xuyw <xuyw@xuyw-PC>
2016-12-09 10:28:37 +0800

    提交

--- src/main/java/com/ai/yk/protal/web/content/mycustomized/MyCustomizedVo.java
+++ src/main/java/com/ai/yk/protal/web/content/mycustomized/MyCustomizedVo.java
@@ -1,6 +1,7 @@
 package com.ai.yk.protal.web.content.mycustomized;
 
 import java.io.Serializable;
+import java.util.List;
 
 import com.ai.yk.protal.web.content.area.AreaVo;
 
@@ -14,6 +15,12 @@ public class MyCustomizedVo implements Serializable {
 	private AreaVo province;
 	/** 城市 **/
 	private AreaVo city;
+	/**兴趣列表**/
+	private List<InterestVo> interestList;
+	/**数据源ID**/
+	private String srcId;
+	/**用户id**/
+	private Integer createId;
 	public int getId() {
 		return id;
 	}
@@ -38,4 +45,23 @@ public class MyCustomizedVo implements Serializable {
 	public void setCity(AreaVo city) {
 		this.city = city;
 	}
+	public List<InterestVo> getInterestList() {
+		return interestList;
+	}
+	public void setInterestList(List<InterestVo> interestList) {
+		this.interestList = interestList;
+	}
+	public String getSrcId() {
+		return srcId;
+	}
+	public void setSrcId(String srcId) {
+		this.srcId = srcId;
+	}
+	public Integer getCreateId() {
+		return createId;
+	}
+	public void setCreateId(Integer createId) {
+		this.createId = createId;
+	}
+	
 }
