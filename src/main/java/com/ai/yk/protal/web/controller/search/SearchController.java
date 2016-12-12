db594bdf5d93971c5830cd22e8f572aaa0f0d4b1
houg <hougang_ahut@163.com>
2016-12-12 12:29:22 +0800

    hg

---
+++ src/main/java/com/ai/yk/protal/web/controller/search/SearchController.java
@@ -0,0 +1,29 @@
+package com.ai.yk.protal.web.controller.search;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ai.yk.protal.web.model.user.SSOClientUser;
+import com.ai.yk.protal.web.utils.SessionUtil;
+
+@Controller
+@RequestMapping("/search")
+public class SearchController {
+	
+	 /**
+     * 搜索页
+     */
+    @RequestMapping("/view")
+    public String searchView(Model model){
+    	SSOClientUser clientUser = SessionUtil.getLoginUser();
+    	if(clientUser==null){
+    		model.addAttribute("isLogin", false);
+    	}else{
+    		model.addAttribute("isLogin", true);
+    		model.addAttribute("user", clientUser);
+    	}
+        return "/search/search";
+    }
+
+}
