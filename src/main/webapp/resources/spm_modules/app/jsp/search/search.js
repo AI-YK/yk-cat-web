ecc1ba9c8de31d890837fce48accfe2b152f9d67
zpg <zpg@zpg-PC>
2016-12-13 09:35:46 +0800

    1

--- src/main/webapp/resources/spm_modules/app/jsp/search/search.js
+++ src/main/webapp/resources/spm_modules/app/jsp/search/search.js
@@ -1,86 +1,114 @@
-define('app/jsp/search/search', function (require, exports, module) {
-    'use strict';
-    var $=require('jquery'),  
-    Widget = require('arale-widget/1.2.0/widget'),
-    AjaxController = require('opt-ajax/1.0.0/index');
-    var Dialog = require("optDialog/src/dialog");
-    require('jquery-i18n/1.2.2/jquery.i18n.properties.min');	
-	require("jsviews/jsrender.min");
-	require("bootstrap-paginator/bootstrap-paginator.min");
-	require("opt-paging/aiopt.pagination");
-	require("twbs-pagination/jquery.twbsPagination.min");
-	var SelectUtil = require("app/jsp/search/select");
-    //实例化AJAX控制处理对象
-    var ajaxController = new AjaxController();
-    
-    var selectUtil = new SelectUtil();
+define(
+		'app/jsp/search/search',
+		function(require, exports, module) {
+			'use strict';
+			var $ = require('jquery'), Widget = require('arale-widget/1.2.0/widget'), AjaxController = require('opt-ajax/1.0.0/index');
+			var Dialog = require("optDialog/src/dialog");
+			require('jquery-i18n/1.2.2/jquery.i18n.properties.min');
+			require("jsviews/jsrender.min");
+			require("bootstrap-paginator/bootstrap-paginator.min");
+			require("opt-paging/aiopt.pagination");
+			require("twbs-pagination/jquery.twbsPagination.min");
+			var SelectUtil = require("app/jsp/search/select");
+			// 实例化AJAX控制处理对象
+			var ajaxController = new AjaxController();
 
-    var searchPage = Widget.extend({
-        //属性，使用时由类的构造函数传入
-        attrs: {
-            clickId:""
-        },
-        //事件代理
-        events: {
-          
-        },
+			var selectUtil = new SelectUtil();
 
-        //重写父类
-        setup: function () {
-        	var _this = this;
-        	searchPage.superclass.setup.call(this);
-            
-            _this.search("news");
-           
-        },
-        _getSearchParams:function(mediaType){
-        	var param = {};
-        	param.mediaType = mediaType;
-        	return param;
-        },
-        /**媒体类型news/social**/
-        search:function(mediaType){
-        	var _this = this;
-			var url = _base +"/news/getSearchPublicSafety";
-			var param = null;
-			var pagination;
-			var renderId = null;
-			var messageId = null;
-			if('news'==mediaType){
-				param = _this._getSearchParams('news');
-				pagination = $("#news-paging");
-				renderId = 'news-list';
-				messageId = 'news-message';
-			}else{
-				param = _this._getSearchParams('social');
-				pagination = $("#social-paging");
-				renderId = 'social-list';
-				messageId = 'social-message';
-			}		
-			pagination.runnerPagination({
-				url:url,
-				method: "POST",
-				dataType: "json",
-				messageId:messageId,
-				renderId:renderId,
-				data : param,
-				pageSize: 8,
-				visiblePages:7,
-				first:false,
-				last:false,
-				message: "正在为您查询数据..",
-				render: function (data) {
-					if('news'==mediaType){
-					   var listHtml = $("#levelNewsTempl").render(data);
-					   $("#news-list").html(listHtml);
-					}else if('social'==mediaType){
-						 var listHtml = $("#levelNewsTempl").render(data);	
-						 $("#social-list").html(listHtml);
+			var searchPage = Widget.extend({
+				// 属性，使用时由类的构造函数传入
+				attrs : {
+					clickId : ""
+				},
+				// 事件代理
+				events : {
+
+				},
+
+				// 重写父类
+				setup : function() {
+					var _this = this;
+					searchPage.superclass.setup.call(this);
+
+					_this._bindEvent();
+					_this.search("news");
+					_this.search("social");
+
+				},
+				_bindEvent : function() {
+					$(".level-left-table ul li a").click(function() {
+						$(".level-left-table ul li a").each(function() {
+							$(this).removeClass("current");
+						});
+						$(this).addClass("current");
+						var index = $('.level-left-table ul li a').index(this);
+						if (index == 0) {
+							$('#le-tba1').show();
+							$('#le-tba2').hide();
+						}
+						if (index == 1) {
+							$('#le-tba2').show();
+							$('#le-tba1').hide();
+						}
+					});
+				},
+				_getSearchParams : function(mediaType) {
+					var param = {};
+					param.mediaType = mediaType;
+					return param;
+				},
+				/** 媒体类型news/social* */
+				search : function(mediaType) {
+					var _this = this;
+					var url = _base + "/news/getSearchPublicSafety";
+					var param = null;
+					var pagination;
+					var renderId = null;
+					var messageId = null;
+					if ('news' == mediaType) {
+						param = _this._getSearchParams('news');
+						pagination = $("#news-paging");
+						renderId = 'news-list';
+						messageId = 'news-message';
+					} else {
+						param = _this._getSearchParams('social');
+						pagination = $("#social-paging");
+						renderId = 'social-list';
+						messageId = 'social-message';
 					}
+					pagination.runnerPagination({
+						url : url,
+						method : "POST",
+						dataType : "json",
+						messageId : messageId,
+						renderId : renderId,
+						data : param,
+						pageSize : 8,
+						visiblePages : 7,
+						first : false,
+						last : false,
+						message : "正在为您查询数据..",
+						callback:function(data){
+							//alert(JSON.stringify(data));
+							if ('news' == mediaType) {
+								$("#news-num").html(data.count);
+							}else if ('social' == mediaType) {
+								$("#social-num").html(data.count);
+							}
+						},
+						render : function(data,count) {
+							if ('news' == mediaType) {
+								$("#news-num").val();
+								var listHtml = $("#levelNewsTempl").render(data);
+								$("#news-list").html(listHtml);
+							} else if ('social' == mediaType) {
+								var listHtml = $("#levelNewsTempl").render(data);
+								$("#social-list").html(listHtml);
+							}
+						}
+					});
 				}
 			});
-        }
-    });
 
-    module.exports = searchPage;
-});
+			module.exports = searchPage;
+		});
