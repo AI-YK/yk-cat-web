aa1782b862645b67052b427744a47d202b73a653
xuyw <xuyw@xuyw-PC>
2016-12-14 14:30:19 +0800

    修改

--- src/main/test/mycustomized/MycustomizedTest.java
+++ src/main/test/mycustomized/MycustomizedTest.java
@@ -33,8 +33,8 @@ public class MycustomizedTest {
 	}
 
 	public static void main(String[] args) {
-		MycustomizedTest.testList();
-		//MycustomizedTest.testDeatil();
+		//MycustomizedTest.testList();
+		MycustomizedTest.testDeatil();
 	}
 
 }
