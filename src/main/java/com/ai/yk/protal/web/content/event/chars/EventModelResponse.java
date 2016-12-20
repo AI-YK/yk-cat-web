808b07d96e3e91bd8536fddbb23f3480e09c349c
xuyw <xuyw@xuyw-PC>
2016-12-20 20:05:41 +0800

    修改返回类型

--- src/main/java/com/ai/yk/protal/web/content/event/chars/EventModelResponse.java
+++ src/main/java/com/ai/yk/protal/web/content/event/chars/EventModelResponse.java
@@ -3,25 +3,24 @@ package com.ai.yk.protal.web.content.event.chars;
 import java.io.Serializable;
 import java.util.List;
 
+import com.alibaba.fastjson.JSONObject;
+
 public class EventModelResponse implements Serializable {
 
 	private static final long serialVersionUID = 5751903476767830545L;
-	private List<SpreadTrendVo> spreadTrend;
+	private JSONObject spreadTrend;
 	private List<TimeTrendVo> timeTrend;
-
-	public List<SpreadTrendVo> getSpreadTrend() {
+	public JSONObject getSpreadTrend() {
 		return spreadTrend;
 	}
-
-	public void setSpreadTrend(List<SpreadTrendVo> spreadTrend) {
+	public void setSpreadTrend(JSONObject spreadTrend) {
 		this.spreadTrend = spreadTrend;
 	}
-
 	public List<TimeTrendVo> getTimeTrend() {
 		return timeTrend;
 	}
-
 	public void setTimeTrend(List<TimeTrendVo> timeTrend) {
 		this.timeTrend = timeTrend;
 	}
+    
 }
