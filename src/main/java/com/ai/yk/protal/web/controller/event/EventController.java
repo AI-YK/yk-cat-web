29c2f91c80f87420b0bbb84ad33e325da326dc7d
xuyw <xuyw@xuyw-PC>
2016-12-22 11:39:32 +0800

    提交

--- src/main/java/com/ai/yk/protal/web/controller/event/EventController.java
+++ src/main/java/com/ai/yk/protal/web/controller/event/EventController.java
@@ -1,6 +1,8 @@
 package com.ai.yk.protal.web.controller.event;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,13 +12,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.ai.opt.base.vo.PageInfo;
+import com.ai.opt.sdk.util.CollectionUtil;
 import com.ai.opt.sdk.util.DateUtil;
+import com.ai.opt.sdk.web.model.ResponseData;
+import com.ai.yk.protal.web.content.ResponseHead;
 import com.ai.yk.protal.web.content.YJRequest;
 import com.ai.yk.protal.web.content.YJResponse;
 import com.ai.yk.protal.web.content.event.EventDetailsMessage;
 import com.ai.yk.protal.web.content.event.EventVo;
+import com.ai.yk.protal.web.content.getInformationList.GetInformationListMessage;
+import com.ai.yk.protal.web.content.getInformationList.GetInformationListReponse;
+import com.ai.yk.protal.web.content.getInformationList.GetInformationListVo;
 import com.ai.yk.protal.web.controller.BaseController;
 import com.ai.yk.protal.web.service.eventdata.EventDataService;
+import com.ai.yk.protal.web.service.getInformationList.GetInformationListService;
+import com.alibaba.fastjson.JSON;
 @RestController
 @RequestMapping("event")
 public class EventController extends BaseController {
@@ -26,6 +37,8 @@ public class EventController extends BaseController {
 	private static final String EVENT_DETAILS_VIEW ="/event/eventDetail";
 	@Autowired
 	private EventDataService eventService;
+	@Autowired
+	private GetInformationListService informationListService;
 	@RequestMapping("detail/{srcId}")
 	public ModelAndView eventDeatil(YJRequest<EventDetailsMessage> req,@PathVariable String srcId) {
 		LOG.info("---------事件详情-【"+srcId+"】-----------");
@@ -43,5 +56,39 @@ public class EventController extends BaseController {
 		view.addObject("begin", begin);
 		view.addObject("end", end);
 		return view;
+	}
+	/**
+	 * 查询事件资讯
+	 * @param req
+	 * @param msg
+	 * @return
+	 */
+	@RequestMapping("queryEventInformation")
+	public ResponseData<PageInfo<GetInformationListVo>> queryEventInformation(YJRequest<GetInformationListMessage> req, GetInformationListMessage msg){
+		if(msg.getPageSize()==null){
+			msg.setPageSize(4);
+		}
+		if(msg.getPageNo()==null){
+			msg.setPageNo(1);
+		}
+		req.setMessage(msg);
+		LOG.info("事件相关资讯："+JSON.toJSONString(req));
+		YJResponse<GetInformationListReponse> res = informationListService.getGetInformationList(req);
+		ResponseHead responseHead = res==null?null:res.getHead();
+		String result = responseHead==null?"false":responseHead.getResult();
+		String resultMsg = responseHead==null?"查询相关资讯失败":responseHead.getMessage();
+		GetInformationListReponse data = res==null?null:res.getData();
+		Integer resultCount = 0;
+		List<GetInformationListVo> resultList = new ArrayList<>();
+		if("true".equals(result) && data!=null&&!CollectionUtil.isEmpty(data.getResultList())){
+			resultCount = data.getResultCount();
+			resultList = data.getResultList();
+		}
+		PageInfo<GetInformationListVo> pageInfo = new PageInfo<GetInformationListVo>();
+		pageInfo.setPageNo(msg.getPageNo());
+		pageInfo.setPageSize(msg.getPageSize());
+		pageInfo.setCount(resultCount);
+		pageInfo.setResult(resultList);
+		return new ResponseData<PageInfo<GetInformationListVo>>(ResponseData.AJAX_STATUS_SUCCESS,resultMsg,pageInfo);
 	}
 }
