a26210e0939ca8205a6d0b3c7070e9e8378c78d9
zpg <zpg@zpg-PC>
2016-12-23 21:30:17 +0800

    zpg

--- src/main/webapp/resources/spm_modules/app/jsp/search/search.js
+++ src/main/webapp/resources/spm_modules/app/jsp/search/search.js
@@ -284,15 +284,6 @@ define(
 				        t += l[i] + ((i + 1) % 3 == 0 && (i + 1) != l.length ? "," : "");  
 				    }  
 				    return t.split("").reverse();  
-				}
-		        _fdigit:function (s) {  
-				    s = parseFloat((s + "").replace(/[^\d\.-]/g, "")) + "";  
-				    var l = s.split(".")[0].split("").reverse();  
-				    var t = "";  
-				    for (i = 0; i < l.length; i++) {  
-				        t += l[i] + ((i + 1) % 3 == 0 && (i + 1) != l.length ? "," : "");  
-				    }  
-				    return t.split("").reverse();  
 				}  
 			});
 
