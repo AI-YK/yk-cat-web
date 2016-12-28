a98b1cd7393f2bd2f821b72fac3765ecf825980c
houg <hougang_ahut@163.com>
2016-12-28 22:49:22 +0800

    hg

--- src/main/java/com/ai/yk/protal/web/filter/AssembleUserInfoFilter.java
+++ src/main/java/com/ai/yk/protal/web/filter/AssembleUserInfoFilter.java
@@ -11,11 +11,16 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.ai.yk.protal.web.model.user.SSOClientUser;
 import com.ai.yk.protal.web.utils.SessionUtil;
 
 
 public class AssembleUserInfoFilter implements Filter {
+	
+	private static final Logger log = LoggerFactory.getLogger(AssembleUserInfoFilter.class);
     private String[] ignor_suffix = {};
     private String ignorSuffixStr = "";
    
@@ -35,9 +40,11 @@ public class AssembleUserInfoFilter implements Filter {
         if (shouldFilter(req)) {
             SSOClientUser user = SessionUtil.getLoginUser(req);
             if (user == null) {
+            	log.info("用户未登录。。。。。。。。。。。。。。。");
             	resp.sendRedirect(req.getContextPath()+"/user/login");
             	return;
             } 
+            log.info("用户已登录。。。。。。。。。。。。。。。");
         }
         chain.doFilter(req, response);
     }
