ea13258b668197b99c750bc490ca70c99ceb5fff
lixiang <lixiang@lixiang-PC>
2017-01-07 20:38:13 +0800

    lx

--- src/main/webapp/resources/spm_modules/app/jsp/common/common.js
+++ src/main/webapp/resources/spm_modules/app/jsp/common/common.js
@@ -6,10 +6,19 @@ define("app/jsp/common/common", function(require, exports, module) {
 		var a = $("#menu li a.current");
 		
 		var html = [];
-		html.push('<a href="'+_base+'">首页</a>');
-        if(a.html()!='首页'){
-        	html.push('> <a href="'+a.attr("href")+'">'+a.html()+'</a>');	
+		if(currentLan=="zh_CN"){
+			html.push('<a href="'+_base+'">首页</a>');
+	        if(a.html()!='首页'){
+	        	html.push('> <a href="'+a.attr("href")+'">'+a.html()+'</a>');	
+			}
+		}else if(currentLan=="en_US"){
+			html.push('<a href="'+_base+'">Home</a>');
+	        if(a.html()!='Home'){
+	        	html.push('> <a href="'+a.attr("href")+'">'+a.html()+'</a>');	
+			}
 		}
+		
+		
 		$("#p_subnav").html(html.join(""));
 	};
 });
